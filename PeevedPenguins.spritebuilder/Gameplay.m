c669841607131119eec8dc6d0135371d771ad0a2
Ruben Ascencio <rubena@amco.me>
2014-07-06 21:21:02 -0700

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Gameplay.m
+++ PeevedPenguins.spritebuilder/Gameplay.m
@@ -20,6 +20,8 @@
     self.userInteractionEnabled = TRUE;
     CCScene *level = [CCBReader loadAsScene:@"Levels/Level1"];
     [_levelNode addChild:level];
+    // visualize physics bodies & joints
+    _physicsNode.debugDraw = TRUE;
 }
 
 - (void)touchBegan:(UITouch *)touch withEvent:(UIEvent *)event
