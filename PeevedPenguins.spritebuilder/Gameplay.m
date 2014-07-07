4bf1ad4d5e74e497735f8a34a8f6fe2dc3065247
Ruben Ascencio <rubena@amco.me>
2014-07-06 22:39:41 -0700

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Gameplay.m
+++ PeevedPenguins.spritebuilder/Gameplay.m
@@ -10,6 +10,8 @@
 #import "CCPhysics+ObjectiveChipmunk.h"
 
 
+static const float MIN_SPEED = 5.f;
+
 @implementation Gameplay  {
     CCPhysicsNode *_physicsNode;
     CCNode *_catapultArm;
@@ -20,7 +22,42 @@
     CCPhysicsJoint *_mouseJoint;
     CCNode *_currentPenguin;
     CCPhysicsJoint *_penguinCatapultJoint;
+    CCAction *_followPenguin;
 }
+
+
+- (void)update:(CCTime)delta
+{
+    // if speed is below minimum speed, assume this attempt is over
+    if (ccpLength(_currentPenguin.physicsBody.velocity) < MIN_SPEED){
+        [self nextAttempt];
+        return;
+    }
+
+    int xMin = _currentPenguin.boundingBox.origin.x;
+
+    if (xMin < self.boundingBox.origin.x) {
+        [self nextAttempt];
+        return;
+    }
+
+    int xMax = xMin + _currentPenguin.boundingBox.size.width;
+
+    if (xMax > (self.boundingBox.origin.x + self.boundingBox.size.width)) {
+        [self nextAttempt];
+        return;
+    }
+}
+
+- (void)nextAttempt
+{
+    _currentPenguin = nil;
+    [_contentNode stopAction:_followPenguin];
+
+    CCActionMoveTo *actionMoveTo = [CCActionMoveTo actionWithDuration:1.f position:ccp(0, 0)];
+    [_contentNode runAction:actionMoveTo];
+}
+
 
 - (void)didLoadFromCCB
 {
@@ -102,8 +139,8 @@
         _currentPenguin.physicsBody.allowsRotation = TRUE;
 
         // follow the flying penguin
-        CCActionFollow *follow = [CCActionFollow actionWithTarget:_currentPenguin worldBoundary:self.boundingBox];
-        [_contentNode runAction:follow];
+        _followPenguin = [CCActionFollow actionWithTarget:_currentPenguin worldBoundary:self.boundingBox];
+        [_contentNode runAction:_followPenguin];
     }
 }
 
